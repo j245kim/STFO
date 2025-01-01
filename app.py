2e16d5dc35b4dbc54c442edce42a2b3fdbc164c5
Seung Hun <negrat@naver.com>
2025-01-01 23:51:10 +0900

    "OOP 및 모듈화 임시완료"

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ if __name__ == '__main__':
 
     cs = crawling_scraping.CrawlingScraping()
     cs.add_website('hankyung')
+    cs.add_website('bloomingbit')
+    cs.add_website('coinreaders')
     cs.add_website('blockstreet')
 
     end_datetime, date_format = '2024-12-31 00:00', '%Y-%m-%d %H:%M'
