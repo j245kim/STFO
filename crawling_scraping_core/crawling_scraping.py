a951d5be283529aa9b1c25bce0aa2a5436699134
Seung Hun <negrat@naver.com>
2025-01-02 09:22:11 +0900

    "OOP 및 모듈화"

--- crawling_scraping_core/crawling_scraping.py
+++ crawling_scraping_core/crawling_scraping.py
@@ -321,7 +321,7 @@ class CrawlingScraping:
     async def hankyung(
                         end_datetime: str, date_format: str,
                         change_format: str, headers: dict[str, str],
-                        min_delay: int | float = 0.55, max_delay: int | float = 1.55
+                        min_delay: int | float = 1, max_delay: int | float = 2
                         ) -> list[dict[str, str, None]]:
         """hankyung 사이트를 크롤링 및 스크래핑 하는 메소드
 
@@ -408,7 +408,7 @@ class CrawlingScraping:
     async def bloomingbit(
                         end_datetime: str, date_format: str,
                         change_format: str, headers: dict[str, str],
-                        min_delay: int | float = 0.55, max_delay: int | float = 1.55
+                        min_delay: int | float = 1, max_delay: int | float = 2
                         ) -> list[dict[str, str, None]]:
         """bloomingbit 사이트를 크롤링 및 스크래핑 하는 메소드
 
