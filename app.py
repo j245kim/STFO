e401c77b005657ec881a659fb133331757c68aaf
Seung Hun <negrat@naver.com>
2025-01-02 12:35:39 +0900

    "OOP 및 모듈화"

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ from crawling_scraping_core import crawling_scraping
 
 if __name__ == '__main__':
     cs = crawling_scraping.CrawlingScraping(record_log=True)
+    end_datetime, date_format = '2025-01-01 00:00', '%Y-%m-%d %H:%M'
     website_list = ('hankyung', 'bloomingbit', 'coinreaders', 'blockstreet')
     for website in website_list:
         cs.add_website(website=website)
-
-    end_datetime, date_format = '2025-01-01 00:00', '%Y-%m-%d %H:%M'
 
     result = cs.run(end_datetime=end_datetime, date_format=date_format)
     cs.to_json()
